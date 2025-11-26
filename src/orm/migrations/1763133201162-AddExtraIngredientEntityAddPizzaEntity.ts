214abb87ef56799b65a2a9c8900b1fe2943e0ff2
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2025-11-26 14:58:48 +0200

    feat(pizza-list): add correct dto for openapi

--- src/orm/migrations/1763133201162-AddExtraIngredientEntityAddPizzaEntity.ts
+++ src/orm/migrations/1763133201162-AddExtraIngredientEntityAddPizzaEntity.ts
@@ -1,16 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddExtraIngredientEntityAddPizzaEntity1763133201162 implements MigrationInterface {
-    name = 'AddExtraIngredientEntityAddPizzaEntity1763133201162'
+  name = 'AddExtraIngredientEntityAddPizzaEntity1763133201162';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "pizza_list" ("id" SERIAL NOT NULL, "imgUrl" character varying NOT NULL, "name" character varying NOT NULL, "ingredients" text NOT NULL, "calories" integer NOT NULL, "price" integer NOT NULL, "favoritesCount" integer NOT NULL DEFAULT '0', CONSTRAINT "PK_98b86d64a33a0e1d688634045cd" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "extra_ingredients" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "calories" integer NOT NULL, "price" integer NOT NULL, CONSTRAINT "PK_5d16ae462d36e79fcacfe3ea8c9" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "pizza_list" ("id" SERIAL NOT NULL, "imgUrl" character varying NOT NULL, "name" character varying NOT NULL, "ingredients" text NOT NULL, "calories" integer NOT NULL, "price" integer NOT NULL, "favoritesCount" integer NOT NULL DEFAULT '0', CONSTRAINT "PK_98b86d64a33a0e1d688634045cd" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "extra_ingredients" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "calories" integer NOT NULL, "price" integer NOT NULL, CONSTRAINT "PK_5d16ae462d36e79fcacfe3ea8c9" PRIMARY KEY ("id"))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "extra_ingredients"`);
-        await queryRunner.query(`DROP TABLE "pizza_list"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "extra_ingredients"`);
+    await queryRunner.query(`DROP TABLE "pizza_list"`);
+  }
 }
